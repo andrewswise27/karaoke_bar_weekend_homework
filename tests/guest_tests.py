ae09376305d3f5218b89a96ad3674fa6753604a3
Andrew Wise <andrewswise2795@gmail.com>
2022-07-02 22:07:49 +0100

    added more tests

--- tests/guest_tests.py
+++ tests/guest_tests.py
@@ -5,4 +5,11 @@ class TestGuest(unittest.TestCase):
     def setUp(self):
         self.guest = Guest("John", 25, "Panic Room")
     
+    def test_guest_has_name(self):
+        self.assertEqual("John", self.guest.name)
+
+    def test_guest_has_money(self):
+        self.assertEqual(25, self.guest.money)
     
+    def test_guest_has_fav_song(self):
+        self.assertEqual("Panic Room", self.guest.fav_song)
--- tests/room_tests.py
+++ tests/room_tests.py
@@ -4,4 +4,16 @@ from src.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("Room 1 ", ["Panic Room", "Blue Lights", "Crashed the Wedding"], [], 4)
+        self.room = Room("Room 1", ["Panic Room", "Blue Lights", "Crashed the Wedding"], ["John"], 4)
+
+    def test_room_has_name(self):
+        self.assertEqual("Room 1", self.room.room_num)
+
+    def test_room_has_playlist(self):
+        self.assertEqual(["Panic Room", "Blue Lights", "Crashed the Wedding"], self.room.song)
+
+    def test_room_has_guest(self):
+        self.assertEqual(["John"], self.room.guest)
+    
+    def test_room_has_capacity(self):
+        self.assertEqual(4, self.room.capacity)
