91588fe37c9286718e3eab430905be1897c903a4
Andrew Wise <andrewswise2795@gmail.com>
2022-07-02 23:30:13 +0100

    added guests and song to room

--- src/room.py
+++ src/room.py
@@ -5,3 +5,14 @@ class Room:
         self.guest = _guest
         self.capacity = _capacity
         
+    def add_guest_to_room(self, name):
+        if len(self.guest) < 4:
+            self.guest.append(name)
+        return self.guest
+
+    def remove_guest_from_room(self, name):
+        self.guest.remove(name)
+    
+    def add_song_to_room(self, song):
+        self.song.append(song)
+        return self.song
--- tests/room_tests.py
+++ tests/room_tests.py
@@ -4,7 +4,7 @@ from src.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("Room 1", ["Panic Room", "Blue Lights", "Crashed the Wedding"], ["John"], 4)
+        self.room = Room("Room 1", ["Panic Room", "Blue Lights", "Crashed the Wedding"], ["John", "Javier"], 4)
 
     def test_room_has_name(self):
         self.assertEqual("Room 1", self.room.room_num)
@@ -13,7 +13,16 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(["Panic Room", "Blue Lights", "Crashed the Wedding"], self.room.song)
 
     def test_room_has_guest(self):
-        self.assertEqual(["John"], self.room.guest)
+        self.assertEqual(["John", "Javier"], self.room.guest)
     
     def test_room_has_capacity(self):
         self.assertEqual(4, self.room.capacity)
+
+    def test_can_add_guest_to_room(self):
+        self.assertEqual(["John", "Javier", "James"], self.room.add_guest_to_room("James"))
+
+    def test_can_add_song_to_room(self):
+        self.assertEqual(["Panic Room", "Blue Lights", "Crashed the Wedding", "Bad Habits"], self.room.add_song_to_room("Bad Habits"))
+    
+    def test_can_remove_guest_from_room(self):
+        self.assertEqual(["John"], self.room.remove_guest_from_room("Javier"))
