71cd7e7283973476ee1a945249f0cedb08a7f8a5
Andrew Wise <andrewswise2795@gmail.com>
2022-07-02 21:45:14 +0100

    added some things

---
+++ tests/karaoke_bar_tests.py
@@ -0,0 +1,48 @@
+from this import s
+import unittest
+from src.karaoke_bar import KaraokeBar
+from src.room import Room
+from src.guest import Guest
+from src.song import Song
+
+class TestKaraokeBar(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.KaraokeBar = KaraokeBar("Little Karaoke Bar", 4, 1000)
+
+        self.room = Room("Room 1", [], [], 4)
+        self.room2 = Room("Room 2", [], [], 4)
+        self.room3 = Room("Room 3", [], [], 4)
+        self.room4 = Room("Room 4", [], [], 4)
+
+        self.guest = Guest("John", 20, {"Post Malone": "Better Now"})
+        self.guest2 = Guest("Dave", 50, {"Calvin Harris": "One Kiss"})
+        self.guest3 = Guest("Julia", 40, {"Jorja Smith": "Blue Lights"})
+        self.guest4 = Guest("Joseph", 35, {"Childish Gambino": "This is America"})
+        self.guest5 = Guest("Dean", 25, {"George Ezra": "Shotgun"})
+        self.guest6 = Guest("Tom", 55, {"James Arthur": "You Deserve Better"})
+        self.guest7 = Guest("Jackie", 37, {"Panic! at the Disco": "High Hopes"})
+        self.guest8 = Guest("Khalid", 55, {"DJ Khalid": "No Brainer"})
+        self.guest9 = Guest("Nathan", 70, {"Busted": "Air Hostess"})
+        self.guest10 = Guest("Joe", 22, {"Sigma": "Anywhere"})
+        self.guest11 = Guest("Dwayne", 37, {"Kygo": "Born To Be Yours"})
+        self.guest12 = Guest("Justin", 74, {"Sigala": "Just Got Paid"})
+        self.guest13 = Guest("Steve", 90, {"Marshmallo": "Happier"})
+
+        self.song = Song("Post Malone", "Better Now")
+        self.song2 = Song("Calvin Harris", "One Kiss")
+        self.song3 = Song("Jorja Smith", "Blue Lights")
+        self.song4 = Song("Childish Gambino", "This is America")
+        self.song5 = Song("George Ezra", "Shotgun")
+        self.song6 = Song("James Arthur", "You Deserve Better")
+        self.song7 = Song("Panic! at the Disco", "High Hopes")
+        self.song8 = Song("DJ Khalid", "No Brainer")
+        self.song9 = Song("Busted", "Air Hostess")
+        self.song10 = Song("Sigma", "Anywhere")
+        self.song11= Song("Kygo", "Born To Be Yours")
+        self.song12 = Song("Sigala", "Just Got Paid")
+        self.song13 = Song("Marshmallo", "Happier")
+        
+
+        
